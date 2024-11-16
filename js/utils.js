af6889f9ff9030e84e4eaed82638bb6175a5553c
Mike Kraus <mike.kraus@mollie.com>
2024-11-16 15:12:37 +0100

    add ipad

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,28 @@
+// Device detection utility functions
 export function isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    return isPhone() || isTablet();
+}
+
+export function isPhone() {
+    return /Android(?!.*Tablet)|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+export function isTablet() {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Detect iPads (both pre-iPadOS 13 and post-iPadOS 13, including desktop-mode iPads)
+    const isIPad = /iPad/i.test(userAgent) || 
+                   (/Macintosh/i.test(userAgent) && navigator.maxTouchPoints && navigator.maxTouchPoints > 1);
+
+    // Detect Android tablets
+    const isAndroidTablet = /Android/i.test(userAgent) && !/Mobile/i.test(userAgent);
+
+    // Generic "Tablet" keyword check
+    const isGenericTablet = /Tablet/i.test(userAgent);
+
+    return isIPad || isAndroidTablet || isGenericTablet;
+}
+
+export function isDesktop() {
+    return !isMobile();
 }
